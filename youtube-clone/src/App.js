89777c5788b9b48037892b5a66577e8e32c7b163
JOSÉ DAVID <jose51949@gmail.com>
2020-12-01 13:53:19 -0600

    Header image

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Header from './Header'
+import Header from './Header';
+import MenuIcon from '@material-ui/icons/Menu'
 
 function App() {
   return (
     <div className="App">
 
       <Header />
+      
       {/*Header*/}
       {/*Sidebar*/}
       {/*Content*/}
--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import VideoCallIcon from '@material-ui/icons/VideoCall'
 
 function Header() {
     return (
         <div>
-            <h1>I'm the header 🎢 🚀</h1>
+            <h1>I'm a header 🎢 🚀</h1>
+            <MenuIcon />
+            <img className="header_logo" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt="Youtube Logo" />
+            <input type="text" />
+            <SearchIcon />
         </div>
+
     )
 }
 
