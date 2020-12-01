ff94b0651df6539ce9892ca2337b4598bd3c0a45
JOSÉ DAVID <jose51949@gmail.com>
2020-12-01 11:56:17 -0600

    Material UI Core and Icons

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTTMrgbImkKl-fzAgFuY5qzb6AqYM3mJo",
+    authDomain: "clone-d7e28.firebaseapp.com",
+    databaseURL: "https://clone-d7e28.firebaseio.com",
+    projectId: "clone-d7e28",
+    storageBucket: "clone-d7e28.appspot.com",
+    messagingSenderId: "715257000241",
+    appId: "1:715257000241:web:b9f9055a841f8f7a9a0010",
+    measurementId: "G-DL4CBVMV8Z"
+  };
